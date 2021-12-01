184d1da58923019a129f6641976f5dff39a2e0bc
difcTechnology <difc.technology@gmail.com>
2021-12-01 18:14:46 -0500

    Prueba

---
+++ app/src/main/java/co/gov/misiontic/ciclo4/MainActivity.kt
@@ -0,0 +1,11 @@
+package co.gov.misiontic.ciclo4
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.linear_layout_3)
+    }
+}
